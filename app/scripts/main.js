f220bf704a10037c2f45e56eded4b56199c69158
Jared Stevick <jstevick@gmail.com>
2015-04-09 18:27:56 -0400

    moved resume to separate html. fixed some styling issues. added tumblr content

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,10 @@
 'use strict';
 
+$(document).ready(function(){
+
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
-
 
 $(function() {
   $('a[href*=#]:not([href=#portfolioModal1], [href=#portfolioModal2], [href=#portfolioModal3], [href=#portfolioModal4], [href=#portfolioModal5], [href=#portfolioModal6])').click(function() {
@@ -20,9 +21,18 @@ $(function() {
   });
 });
 
-
+$(window).scroll(function() {
+    if($(this).scrollTop() > 350) {
+        $('.navbar-fixed-top').addClass('opaque');
+    } else {
+        $('.navbar-fixed-top').removeClass('opaque');
+    }
+});
 
 $('#submitForm').on('click', function(){
+	alert("Thanks for getting in touch. I look forward to talking with you!");
     $('#contactForm')[0].reset();
+});
+
 });
 
